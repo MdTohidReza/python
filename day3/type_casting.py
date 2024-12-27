2df9b3baba0d96ab0ca7370fc3fd69993fcd2212
MdTohidReza <harishalone786@gmail.com>
2024-12-27 21:38:37 +0545

    type casting

---
+++ day3/type_casting.py
@@ -0,0 +1,21 @@
+pie = 3.14
+print(pie)
+print(type(pie))
+print(int(pie))
+print(type(str(pie)))
+
+
+fruits=(
+    'apple',
+    'orange',
+    'kiwi',
+    'mango',
+    'banana',
+    123,
+    1.34,
+)
+
+fruits = list(fruits)
+fruits[0]='mango'
+print(tuple(fruits))
+# print(type(fruits))

0ada8a5bd1a54cd6e26031e36fe3520e5c0bf700
MdTohidReza <harishalone786@gmail.com>
2024-12-27 21:41:38 +0545

    string

--- day 2/string.py
+++ day 2/string.py
@@ -1,11 +1,12 @@
-name = "Tohid"
-print(name)
-print(type(name))
+# name = "Tohid"
+# print(name)
+# print(type(name))
 
 city='''haidher zulfikar'''
 # print(city)
 
 # string lie re assignment grna mildai na
-name[0]='s'
-print(name)
-# print(city[-1])
+# name[0]='s'
+# print(name)
+name = ''' Tohid Hello'''
+print(name[::-1])

47524545cb62e2adf13d0108ea397280ad4337dc
MdTohidReza <harishalone786@gmail.com>
2024-12-31 21:42:52 +0545

    loops

---
+++ day 4/loops.py
@@ -0,0 +1,15 @@
+#  while loop
+
+# while True:
+#     print("enter your name")
+#     name = (input())
+#     if name =='stop':
+#         break
+    
+    
+i=0
+while i<=10:
+    print(f" {i}Hello")
+    if i==5:
+        break
+    i+=1

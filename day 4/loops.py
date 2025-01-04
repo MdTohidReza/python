1bd7ed0a9a6ff7176c8a8e51f2e8f3266272902f
MdTohidReza <harishalone786@gmail.com>
2025-01-04 21:56:34 +0545

    loops

--- day 4/loops.py
+++ day 4/loops.py
@@ -1,38 +1,56 @@
-#  while loop
+# #  while loop
 
-while True:
-    print("enter your name")
-    name = (input())
-    if name =='stop':
-        break
+# while True:
+#     # print("enter your name")
+#     name = (input())
+#     if name =='stop':
+#         break
 
 
 
-i=0
-while i<=10:
-    print(f" {i}Hello")
-    if i==5:
-        break
-    i+=1
+# i=0
+# while i<=10:
+#     # print(f" {i}Hello")
+#     if i==5:
+#         break
+#     i+=1
     
     
-# for loop
+# # for loop
 
-fruits=['apple','banana','mango','orange',123,1.2]
-print("apples" in fruits)
-for index ,fruit in enumerate(fruits):
-    print(index+1,fruit)
-
-
-# range function
-
-print(type(range(10)))
-
-numbers = list(range(1,10,2))
-print(numbers)
+# fruits=['apple','banana','mango','orange',123,1.2]
+# print("apples" in fruits)
+# for index ,fruit in enumerate(fruits):
+#     print(index+1,fruit)
 
 
-for i in range(10):
-    print(i)
-    if i==3:
-        break
+# # range function
+
+# print(type(range(10)))
+
+# numbers = list(range(1,10,2))
+# print(numbers)
+
+
+# for i in range(10):
+#     # print(i)
+#     if i==3:
+#         break
+
+
+#  To print even numbers
+# for i in range(1,21):
+#    if i%2==0:
+#     print(f"{i} is even number")
+#    else:
+#     print(f"{i} is odd number")
+    
+#  task both using for and while loop
+# 1
+# 12
+# 123
+
+
+words= "hello world"
+for char in words:
+    print(char)
